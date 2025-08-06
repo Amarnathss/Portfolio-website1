5e2089f57c0d969e02ffe50ad9aac2a05d593188
Amarnath S S <amarnathss306@gmail.com>
2025-08-06 22:15:20 +0530

    Update hero-section.tsx

--- client/src/components/hero-section.tsx
+++ client/src/components/hero-section.tsx
@@ -15,7 +15,7 @@ export default function HeroSection() {
           </p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <a
-              href="https://drive.google.com/file/d/1kq_QBcdQ92Zn0biYN3hYsgttzKjjfXFg/view?usp=sharing"
+              href="https://drive.google.com/file/d/1XuhlXyijssGEimiL_CygBqaOG0RWE4wC/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="chalk-hover chalk-box px-6 py-3 chalk-text font-chalk-body text-lg font-semibold hover:bg-chalk hover:text-black transition-all duration-300"
